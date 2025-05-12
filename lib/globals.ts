43b2e0d19b749b849aec090d85391a96db8af9e6
jofflin <tech@hoefflin.io>
2025-05-12 17:24:58 +0200

    update address

--- lib/globals.ts
+++ lib/globals.ts
@@ -1,4 +1,4 @@
-export const VENUE = "Gerhard-Graf-Sporthalle, Katharinenstraße 33, 79111 Freiburg";
+export const VENUE = "Gerhard-Graf-Sporthalle, Friedrichstraße 51, 79098 Freiburg";
 export const URL = "https://red-sparrows.getnono.de";
 export const HOMEPAGE_URL = "https://redsparrows.de";
 export const TITLE = "Red Sparrows Ticketshop";

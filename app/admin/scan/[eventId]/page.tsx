f77d6ca08029e79dcc17a694eb0ba3c2dda551ce
jofflin <tech@hoefflin.io>
2025-05-16 10:49:57 +0200

    updated scanning interface

--- app/admin/scan/[eventId]/page.tsx
+++ app/admin/scan/[eventId]/page.tsx
@@ -36,7 +36,7 @@ export default async function ScanPage({ params }: { params: { eventId: string }
   return (
     <div className="min-h-screen">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <header className="py-6">
+        <header className="pt-6">
           <div className="flex items-center justify-between">
             <div className="flex items-center">
               <Button asChild variant="ghost" size="icon" className="hover:bg-gray-200">
@@ -44,11 +44,9 @@ export default async function ScanPage({ params }: { params: { eventId: string }
                   <ArrowLeft className="h-4 w-4" />
                 </Link>
               </Button>
-              <h1 className="text-2xl font-bold text-gray-900">Ticket Scan</h1>
+              <h1 className="text-2xl font-bold text-gray-900">{event.name}</h1>
             </div>
           </div>
-          <CardDescription className="text-sm">{`${event.name} - ${moment.tz(event.start_time, "Europe/Berlin").format("HH:mm")} Uhr`}</CardDescription>
-          <CardDescription className="text-sm">Noch {tickets.filter((t) => !t.redeemed_at).length} Tickets zu entwerten</CardDescription>
         </header>
         <main className="">
           <AdminTicketScanComponent
